5f3846d510e86cecf8b2317bba0a9cb3ce0e4b2b
Hadrami <abdrahmane201@gmail.com>
2024-01-01 19:53:00 +0000

    version after presentation 26 novembre 2023

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 
-const baseURL = "http://localhost:8000"
-//const baseURL = "https://mgmsogem.binorassocies.com/api"
+//const baseURL = "http://localhost:8000"
+const baseURL = "https://mgmsogem.binorassocies.com/api"
 
 export default baseURL;
